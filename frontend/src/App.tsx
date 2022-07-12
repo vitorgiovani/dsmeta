2aad86f7e2fa0693ba6d98e3171c763befbc4ce8
Vitor Giovani <vitorxgiovani@gmail.com>
2022-07-12 10:51:39 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
-    function App() {
-  return(
-    <h1>Olá mundo!</h1>
+import NotificationButton from "./componentes/NotificationButton"
+
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
