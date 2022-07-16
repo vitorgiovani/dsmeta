255f8e5304d6bcb8d6dd7a0132f57a2f855d8166
Vitor Giovani <vitorxgiovani@gmail.com>
2022-07-15 23:50:23 -0300

    atualizacao

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
@@ -44,7 +44,7 @@ public class SmsService {
 
 		Message message = Message.creator(to, from, msg).create();
 
-		System.out.println(message.getSid());
+		System.out.println(message.getSid());	
 	}
 }
 
